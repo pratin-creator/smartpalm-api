e3f20b6e66b073d2a5ee344e4cf51facc4026ff3
smartpalm <pratin@kts.ac.th>
2026-06-27 23:01:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -300,3 +300,7 @@ async def predict_v1(file: UploadFile = File(...)):
             },
 
         )
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=10000)
