3981075fa080c299d5e8e89f9e2f8a7e07c93d55
smartpalm <pratin@kts.ac.th>
2026-06-27 22:39:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from tensorflow.keras.models import load_model
+from huggingface_hub import hf_hub_download
 from tensorflow.keras.utils import load_img, img_to_array
 import numpy as np
 import io
@@ -13,7 +14,8 @@ from typing import Optional
 # Basic Config
 # =========================
 
-MODEL_PATH = "smartpalm_mobilenetv2.h5"
+HF_REPO = "smartpalmai/SmartPalm-MobileNetV2"
+HF_FILENAME = "smartpalm_mobilenetv2.h5"
 MODEL_NAME = "MobileNetV2"
 MODEL_VERSION = "v1.2"
 API_VERSION = "1.2"
@@ -53,7 +55,14 @@ def get_model():
     global model
     if model is None:
         logger.info("Loading AI model...")
+        print("Downloading model from Hugging Face...")
+        MODEL_PATH = hf_hub_download(
+            repo_id=HF_REPO,
+            filename=HF_FILENAME
+        )
+        print("Loading model...")
         model = load_model(MODEL_PATH)
+        print("Model Loaded Successfully")
         logger.info("Model loaded successfully.")
     return model
 
@@ -292,5 +301,5 @@ async def predict_v1(file: UploadFile = File(...)):
 <<<<<<< HEAD
         )
 =======
-        )
+        
 >>>>>>> f13f991a10428fe874d138312546cbeaf7508e2a
